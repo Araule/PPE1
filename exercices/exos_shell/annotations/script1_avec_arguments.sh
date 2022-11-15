2ee30a22c7259edd459429bc3937f085f8a899f1
Araule <lau7tsu@gmail.com>
2022-11-15 15:15:42 +0100

    sup dossier en trop

--- exercices/exos_shell/annotations/script1_avec_arguments.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-ANNEE=$1
-
-# avec >> on rajouter des données dans sortie.txt
-echo "pour l'année $ANNEE" >> sortie.txt
-grep "" $ANNEE*.ann | wc -l >> sortie.txt
--- exercices/exos_shell/locations/script2_avec_arguments.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-ANNEE=$1
-TYPE=$2
-
-# avec >> on rajouter des données dans sortie.txt
-echo "pour l'année $ANNEE" >> sortie2.txt
-grep "$TYPE" $ANNEE*.ann | wc -l >> sortie2.txt
